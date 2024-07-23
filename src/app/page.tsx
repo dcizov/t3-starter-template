aa8fe585d5354c8fe1ad86817c322b98ced4d306
wtfaq <dimitricizov30@gmail.com>
2024-07-23 16:49:11 +0200

    feat: add homepage with mode-toggle for theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,71 @@
 import Link from "next/link";
-
-import { LatestPost } from "@/app/_components/post";
+import { PanelsTopLeft } from "lucide-react";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { Button } from "@/app/_components/ui/button";
+import { ModeToggle } from "@/app/_components/mode-toggle";
 import { getServerAuthSession } from "@/server/auth";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
-
-  void api.post.getLatest.prefetch();
+  const year = new Date().getFullYear();
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+      <div className="flex min-h-screen flex-col">
+        <header className="sticky top-0 z-[50] w-full border-b border-border/40 bg-background/95 backdrop-blur-sm dark:bg-black/[0.6]">
+          <div className="container flex h-14 items-center">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
+              href="/"
+              className="flex items-center justify-start transition-opacity duration-300 hover:opacity-85"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
+              <PanelsTopLeft className="mr-3 h-6 w-6" />
+              <span className="font-bold">Acme Inc</span>
+              <span className="sr-only">Acme Inc</span>
             </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+            <nav className="ml-auto flex items-center gap-2">
+              <ModeToggle />
+            </nav>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+        </header>
+        <main className="flex flex-1 items-center justify-center">
+          <div className="container relative pb-10">
+            <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
+              <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                This is the start of something new
+              </h1>
+              <span className="max-w-[750px] text-center text-lg font-light text-foreground">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea.
+              </span>
+              <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <Button variant="default" asChild>
+                  <Link
+                    href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                  >
+                    {session ? "Sign out" : "Sign in"}
+                    <ArrowRightIcon className="ml-2" />
+                  </Link>
+                </Button>
+                <Button variant="outline" asChild>
+                  <Link href={session ? "/dashboard" : "/api/auth/signup"}>
+                    {session ? "Dashboard" : "Sign up"}
+                  </Link>
+                </Button>
+              </div>
+            </section>
+          </div>
+        </main>
+        <footer className="border-t border-border/40 py-6 md:py-0">
+          <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+            <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
+              © Acme Inc {year}, All rights reserved.
             </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
           </div>
-
-          {session?.user && <LatestPost />}
-        </div>
-      </main>
+        </footer>
+      </div>
     </HydrateClient>
   );
 }
