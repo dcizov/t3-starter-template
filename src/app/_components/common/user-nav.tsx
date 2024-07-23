6bb1d6c608c1ee5565ffd4b33042aaf809cac27d
wtfaq <dimitricizov30@gmail.com>
2024-07-23 21:43:07 +0200

    feat: add user-nav component

---
+++ src/app/_components/common/user-nav.tsx
@@ -0,0 +1,101 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+"use client";
+
+import Link from "next/link";
+import { LayoutGrid, LogOut, User, Settings } from "lucide-react";
+
+import { Button } from "@/app/_components/ui/button";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/app/_components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/app/_components/ui/tooltip";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+import { signOut, useSession } from "next-auth/react";
+
+export function UserNav() {
+  const { data: session } = useSession();
+
+  return (
+    <DropdownMenu>
+      <TooltipProvider disableHoverableContent>
+        <Tooltip delayDuration={100}>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="relative h-8 w-8 rounded-full"
+              >
+                <Avatar className="h-8 w-8">
+                  {session?.user.image ? (
+                    <AvatarImage src={session?.user.image} alt="Avatar" />
+                  ) : (
+                    <AvatarFallback className="bg-transparent">
+                      JD
+                    </AvatarFallback>
+                  )}
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">Profile</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {session?.user.name}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {session?.user.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem className="hover:cursor-pointer" asChild>
+            <Link href="/dashboard" className="flex items-center">
+              <LayoutGrid className="mr-3 h-4 w-4 text-muted-foreground" />
+              Dashboard
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="hover:cursor-pointer" asChild>
+            <Link href="/account" className="flex items-center">
+              <User className="mr-3 h-4 w-4 text-muted-foreground" />
+              Account
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="hover:cursor-pointer" asChild>
+            <Link href="/settings" className="flex items-center">
+              <Settings className="mr-3 h-4 w-4 text-muted-foreground" />
+              Settings
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="hover:cursor-pointer"
+          onClick={() => signOut({ callbackUrl: "/", redirect: true })}
+        >
+          <LogOut className="mr-3 h-4 w-4 text-muted-foreground" />
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
