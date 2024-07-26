cadda237f9bf526aed1aaca54c71b5029822e43a
wtfaq <dimitricizov30@gmail.com>
2024-07-26 10:09:05 +0200

    feat: add auth buttons

---
+++ src/app/_components/auth/auth-button.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/app/_components/ui/button";
+import { LoaderCircle } from "lucide-react";
+
+interface Props {
+  isLoading: boolean;
+  typeSubmit: "signin" | "signup";
+}
+
+export default function AuthSubmitButton({ isLoading, typeSubmit }: Props) {
+  return (
+    <Button type="submit" className="w-full" disabled={isLoading}>
+      {isLoading && (
+        <span className="mr-2 animate-spin">
+          <LoaderCircle size={16} />
+        </span>
+      )}
+      {typeSubmit === "signin" ? "Sign in" : "Create Account"}
+    </Button>
+  );
+}
---
+++ src/app/_components/auth/google-auth-button.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { Button } from "@/app/_components/ui/button";
+import { LoaderCircle } from "lucide-react";
+import { SiGoogle } from "@icons-pack/react-simple-icons";
+
+interface Props {
+  typeSubmit: "signin" | "signup";
+}
+
+export default function GoogleSigninButton({ typeSubmit }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGoogleSignIn = async () => {
+    setIsLoading(true);
+    await signIn("google", { callbackUrl: "/", redirect: true });
+  };
+
+  return (
+    <Button
+      variant="outline"
+      type="button"
+      disabled={isLoading}
+      className="flex w-full items-center justify-center gap-2"
+      onClick={handleGoogleSignIn}
+    >
+      {isLoading ? (
+        <span className="animate-spin">
+          <LoaderCircle size={16} />
+        </span>
+      ) : (
+        <SiGoogle size={16} />
+      )}{" "}
+      {typeSubmit === "signup" ? "Sign Up with Google" : "Sign in with Google"}
+    </Button>
+  );
+}
