69ba1a24ba8770034576d88d9e8ccf0cb11686af
wtfaq <dimitricizov30@gmail.com>
2024-08-20 19:27:05 +0200

    fix: use strict types for helper functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,11 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // Returns a date object based on the current time plus a specified number of seconds
-export function fromDate(time: number, date = Date.now()) {
+export function fromDate(time: number, date: number = Date.now()): Date {
   return new Date(date + time * 1000);
 }
 
 // Determines the user role based on the provided email
-export function getUserRole(email: string) {
+export function getUserRole(email: string): "user" | "admin" {
   return email === process.env.ADMIN_USER_EMAIL ? "admin" : "user";
 }
